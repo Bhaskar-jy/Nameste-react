8b39bcd93c1b5368ab42f771ab96517a566f89b6
Bhaskar-jy <000bhaskarsaikia@gmail.com>
2024-08-13 02:32:00 +0530

    Shimmer UI added

--- src/components/BodyE5.js
+++ src/components/BodyE5.js
@@ -1,30 +1,9 @@
 import RestorantCard from "./RestorantCard";
 import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
-import Shimmer from "./Shimmer";
 const Body = () => {
   //State Variable -> Super powerful variable
-  const [resList1, setresList] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.16363&lng=91.7611838&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-
-    const json = await data.json();
-
-    setresList(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-  };
-
-  if (resList1?.length === 0) {
-    return <Shimmer />;
-  }
+  const [resList1, setresList] = useState(resList);
   return (
     <div className="body">
       <div className="filter">
@@ -33,7 +12,7 @@ const Body = () => {
           onClick={() => {
             console.log("Button Clicked");
             const filteredresList = resList1.filter(
-              (res) => res.info?.avgRating > 4.2
+              (res) => res.info?.avgRating > 4.5
             );
             setresList(filteredresList);
           }}
@@ -42,7 +21,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {resList1?.map((restorant) => (
+        {resList1.map((restorant) => (
           <RestorantCard key={restorant.info.id} resData={restorant} />
         ))}
       </div>
