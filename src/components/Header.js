ffc1c8d48ab76c4090a1d23cdd44f0d2030f343d
Bhaskar-jy <000bhaskarsaikia@gmail.com>
2024-08-22 12:08:50 +0530

    first tailwind comment

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
   const [btnNamefreact, setbtnNamereact] = useState("Login");
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
+    <div className="flex justify-between bg-gray-300 shadow-lg mb-2 px-11">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-40" src={LOGO_URL} />
       </div>
-      <div className="nev-items">
-        <ul>
+      <div className="nev-items flex items-center">
+        <ul className="flex p-4 m-4">
           <li>Online Status: {onlineStatus === true ? "🟢" : "🔴"}</li>
           <li className="list">
             <Link to="/">Home</Link>
